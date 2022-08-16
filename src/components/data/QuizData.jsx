080c22a8dc1234c772a86fed661cb2a512cceb6c
Andrew-McCall <amccall@qa.com>
2022-08-16 16:25:42 +0100

    Fixed answer

--- src/components/data/QuizData.jsx
+++ src/components/data/QuizData.jsx
@@ -1,5 +1,5 @@
 const QUIZDATA = [
-    {title:"Quiz One", description:"Maths", questions:[{Q:"What is 1+1?", A:"2"},{Q:"What is 63+23?", A:"85"}]},
+    {title:"Quiz One", description:"Maths", questions:[{Q:"What is 1+1?", A:"2"},{Q:"What is 63+22?", A:"85"}]},
     {title:"Quiz Two", description:"English", questions:[{Q:"How do you spell Shakespear?", A:"shakespear"},{Q:"What colour is an Orange?", A:"orange"},{Q:"What is a doing word?", A:"verb"}]},
 ]
 
